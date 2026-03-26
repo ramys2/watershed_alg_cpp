db54285d0c57990340f9d8be23dcb4bdda5e408c
Radovan Myslik <rada.myslik@gmail.com>
2026-03-26 23:28:19 +0100

    libs setup

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,39 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Config.hpp>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+int main() {
+    std::cout << "SFML Version: "
+                  << SFML_VERSION_MAJOR << "."
+                  << SFML_VERSION_MINOR << "."
+                  << SFML_VERSION_PATCH << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Pop_OS ImGui + SFML");
+    window.setFramerateLimit(60);
+
+    if (!ImGui::SFML::Init(window)) return -1;
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, Pop_OS!");
+        ImGui::Button("Look at me, I'm a button!");
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
     return 0;
 }
